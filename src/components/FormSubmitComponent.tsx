fd0a0c294ef86328155b0f2e6f6f3b600d0351a4
Gold240sx <240designworks@gmail.com>
2024-04-08 18:36:59 -0500

    FIxed the issue regarding the form height cutting off extra tall forms

--- src/components/FormSubmitComponent.tsx
+++ src/components/FormSubmitComponent.tsx
@@ -72,7 +72,7 @@ const FormSubmitComponent = ({
 
 	if (submitted) {
 		return (
-			<div className="flex justify-center w-full h-full items-center p-8">
+			<div className="flex justify-center w-full items-center p-8">
 				<div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
 					<h1 className="text-2xl font-bold">Form Submitted</h1>
 					<p className="text-muted-foreground">
@@ -85,7 +85,7 @@ const FormSubmitComponent = ({
 	}
 
 	return (
-		<div className="flex justify-center w-full h-full items-center p-8">
+		<div className="flex justify-center w-full items-center p-8">
 			<div
 				key={renderKey}
 				className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
